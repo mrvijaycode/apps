705d205f4eca6ae7731126960e5e706282d4c141
Vijay Bhaskar <vijay.vijaymca@gmail.com>
2015-05-14 18:02:25 +0530

    Update logout.js

--- logout.js
+++ logout.js
@@ -1,14 +1,5 @@
-if (typeof SP !== 'undefined') {
-	var ctx = new SP.ClientContext.get_current();
-	var site = ctx.get_site();
-	ctx.load(site, 'Url');
-	ctx.executeQueryAsync(Function.createDelegate(this, function (sender, args) {
-			var url = site.get_url();
-			window.location = url + '/_layouts/closeConnection.aspx?loginasanotheruser=true';
-		}), Function.createDelegate(this, function (sender, args) {
-			alert('Error: ' + args.get_message());
-		}));
-} else {
-	alert('Error: This is not a SharePoint 2010 or 2013 website\nFor more information please visit: https://epandu.wordpress.com/');
-	window.location.assign("https://epandu.wordpress.com");
+function main()
+{
+alert('welcome to the app');
 }
+main();
